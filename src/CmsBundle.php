ac2ee90d38654b992349fd7d188854d0b4b6c4d7
github-ci <you@example.com>
2025-06-12 06:51:31 +0800

    switch

--- src/CmsBundle.php
+++ src/CmsBundle.php
@@ -3,7 +3,15 @@
 namespace CmsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineAsyncInsertBundle\DoctrineAsyncInsertBundle;
 
-class CmsBundle extends Bundle
+class CmsBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            DoctrineAsyncInsertBundle::class => ['all' => true],
+        ];
+    }
 }
